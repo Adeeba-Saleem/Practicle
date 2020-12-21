e8b35218b00570a64a38611ef2be3919f40417ba
Adeeba-Saleem <adeeadee2345@gmail.com>
2020-12-21 16:45:39 +0530

    adding views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Badge,Col} from 'reactstrap';
+import Event from './components/EventComponents';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="container mt-5">
+            <div className="row">
+              <Col>
+              <h1><Badge color="info">POPULAR GROUPS</Badge> <span dark color="primary">UPCOMING EVENTS</span></h1>
+              </Col>
+            </div>
+          </div>
+          <div className="container mt-5 ">
+            <div className="row">
+               <Event/>
+            </div>
+          </div>
+          
     </div>
   );
 }
---
+++ src/components/EventComponents.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody,
+    CardTitle,CardSubtitle} from 'reactstrap';
+    import { BiCalendarCheck } from "react-icons/bi";
+    import {IoMdTime} from "react-icons/io";
+    import { BsFillPeopleFill } from "react-icons/bs";
+   
+
+class Event extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           events: [
+                {
+                  id: 0,
+                  name:'Beach Cleanup with Friends organized by Habitat Friends',
+                  image: 'assets/images/Joshua-Tree-Yoga-4-1.jpg',
+                  Date: '13th Auguest',
+                  seats:'12',
+                  Time: '4.00pm-6PM',
+                  price:'LKR 2,000',
+                                        },
+               {
+                  id: 1,
+                  name:'Beach Cleanup with Friends organized by Habitat Friends',
+                  image: 'assets/images/new-zealanad.jpg',
+                  Date: '13th Auguest',
+                  seats:'12',
+                  Time: '4.00pm-6PM',
+                  price:'FREE',
+                                        },
+               {
+                  id: 2,
+                  name:'Beach Cleanup with Friends organized by Habitat Friends',
+                  image: 'assets/images/Running-into-the-Water-scaled-e1581370585398.jpg',
+                  Date: '13th Auguest',
+                  seats:'12',
+                  Time: '4.00pm-6PM',
+                  price:'LKR 2,000',                  },
+               {
+                  id: 3,
+                  name:'Beach Cleanup with Friends organized by Habitat Friends',
+                  image: 'assets/images/images.jpg',
+                  Date: '13th Auguest',
+                  seats:'12',
+                  Time: '4.00pm-6PM',
+                  price:'FREE',                        }
+               ],
+        };
+    }
+
+    render() {
+        const event = this.state.events.map((event) => {
+            return (
+                
+              <div key={event.id} className="col-3 mt-3" >
+                <Card >
+                  <CardImg src={event.image} alt={event.name} />
+                  <CardBody >
+                  <CardTitle tag="h5">{event.name}</CardTitle>
+                  <CardSubtitle><BiCalendarCheck/>{event.Date}</CardSubtitle>
+                  <CardSubtitle><IoMdTime/>{event.Time}</CardSubtitle>
+                  <CardSubtitle><BsFillPeopleFill/>{event.seats}</CardSubtitle>
+                  <CardTitle tag="h5">{event.price}</CardTitle>
+                  </CardBody>
+                </Card>
+              </div>
+            
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+                  {event}
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Event;
